6f3082ee55730d31f570ce2c1bf5815a4eb132d8
Apurv Sharma <apurv.py@gmail.com>
2026-07-03 16:03:50 +0530

    Basic oops concepts

---
+++ oops1.py
@@ -0,0 +1,18 @@
+#intiate a class
+class employee:
+    #special method/magic method/dunder method - constructor
+    def __init__(self):
+        self.id = 123
+        self.salary = 50000
+        self.designation = "SDE"
+
+    def travel(self, destination):
+        print(f"Employee is now travelling to {destination}")
+#Create an instance/object of the class
+
+sam = employee()
+#print(sam.id)
+
+
+sam.travel("kerala")
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,7 @@
+lst = [1,2,3]
+mystr = "Mlops playlist"
+my_int = 155
+
+print(type(lst))
+my_mster = mystr.capitalize
+print(my_mster)
