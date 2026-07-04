b22cebdaea91a1d9a26d0ea4a3beff7baa65e7a6
Apurv Sharma <apurv.py@gmail.com>
2026-07-04 13:07:16 +0530

    encapsulation, getter setter and static method covered

--- oops_proj.py
+++ oops_proj.py
@@ -1,10 +1,35 @@
 class chatbook:
+
+    __user_id = 1 #static attibute
     def __init__(self):
+        self.id = chatbook.__user_id #class access static attribute
+        chatbook.__user_id += 1
         self.__name = "Default user"
         self.username = ''
         self.password = ''
         self.loggedin = False
         #self.menu()
+
+    @staticmethod #decorator
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id = val
+    
+
+
+
+
+
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, value):
+        self.__name = value
+
+
     def menu(self):
         user_input = input("""Welcome to chatbook, How would you like to proceed
                            1. Press 1 to signup
--- rough.py
+++ rough.py
@@ -11,9 +11,32 @@
 lst = [1,2,3]
 a1 = len(lst)
 print(a1)
-
-# METHOD
 from oops_proj import chatbook
 user1 = chatbook()
+print(user1.id)
+
+#Using static method directly from class rather than object
+chatbook.set_id(10)
+user2 = chatbook()
+print(user2.id)
+
+user3 = chatbook()
+print(user3.id)
+
+
+
+
+
+
+
+#getter and setter
+# print(user1._chatbook__name)
+# print(user1.get_name())
+# user1.set_name("Apurv")
+# print(user1.get_name())
+
+# METHOD
+#from oops_proj import chatbook
+#user1 = chatbook()
 #user1.sendmsg()
-print(user1._chatbook__name)
+#print(user1._chatbook__name)
